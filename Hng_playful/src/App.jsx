ca64d70afd4d78aad2072111d7aefc692fb09ab3
Raesammurd <olowookererereloluwa@gmail.com>
2024-07-11 17:59:17 -0800

    added some change

--- Hng_playful/src/App.jsx
+++ Hng_playful/src/App.jsx
@@ -20,8 +20,8 @@ function App() {
       <Display/>
       <Footer/>
       <Routes>
-        <Route path="/" element={<Shop/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+        {/* <Route path="/" element={<Shop/>}/>
+        <Route path="/cart" element={<Cart/>}/> */}
 
       </Routes>
     </Router>
